4a3c80b026e28e7a7d90cc606023c8390f8ce352
unknown <muhibnuhsabri@gmail.com>
2026-07-28 00:39:17 +0800

    Portfolio update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 console.log("Portfolio Loaded");
+
+/* =====================================================
+   SCROLL PROGRESS
+===================================================== */
+
+const progressBar = document.getElementById("scroll-progress");
+
+window.addEventListener("scroll", () => {
+
+    const totalHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+
+    const progress =
+        (window.scrollY / totalHeight) * 100;
+
+    progressBar.style.width = progress + "%";
+
+});
